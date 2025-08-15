2cc8c955abaa221a03888022d4a0ff99ae8deb37
Shen <141698141+dsh3n77@users.noreply.github.com>
2025-08-14 20:28:54 -0400

    Update web environment.py

--- src/mav/Tasks/web/environment.py
+++ src/mav/Tasks/web/environment.py
@@ -1,6 +1,13 @@
+from pydantic import BaseModel, Field, ConfigDict
 from mav.Tasks.base_environment import TaskEnvironment
-from pydantic import BaseModel, Field
+from mav.Tasks.web.visualwebarena.browser_env import StateInfo, Trajectory
+from typing import Any, Dict  
+import numpy as np           
+import numpy.typing as npt    
 
-
-class ActionTypes(BaseModel):
-    pass
+Observation = str | npt.NDArray[np.uint8]
+class WebEnvironment(TaskEnvironment):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    state: StateInfo
+    trajectory: Trajectory
+    name: str
